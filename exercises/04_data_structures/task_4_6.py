2a7eb953c218189ae5b3275503431275edd77de0
Ivan <ghostmorgan13@gmail.com>
2021-11-25 21:16:38 +0300

    Решил задание 4.6

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -16,5 +16,17 @@ Outbound Interface    FastEthernet0/0
 Это не значит, что задание сделано правильно, просто на данном этапе сложно иначе
 проверять результат.
 """
+template = """
+Prefix                {:<15}
+AD/Metric             {:<15}
+Next-Hop              {:<15}
+Last update           {:<15}
+Outbound Interface    {:<15}
+"""
 
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+
+route = ospf_route.strip().split()
+route.remove("via")
+prefix, metric, next_hop, last_update, out_int = route
+print(template.format(prefix, metric.strip('[]'), next_hop.strip(','), last_update.strip(','), out_int))
