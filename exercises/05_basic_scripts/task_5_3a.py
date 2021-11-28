2b056b0c9f6d2bb4c84a2717805169d21bb42f81
Ivan <ghostmorgan13@gmail.com>
2021-11-29 01:46:56 +0300

    Решил задание 5.3a

--- exercises/05_basic_scripts/task_5_3a.py
+++ exercises/05_basic_scripts/task_5_3a.py
@@ -25,3 +25,13 @@ trunk_template = [
     "switchport mode trunk",
     "switchport trunk allowed vlan {}",
 ]
+
+mode = {"access": [access_template, "Введите номер VLAN: "], "trunk": [trunk_template, "Введите разрешенные VLANы: "]} # словарь для обхода использования цикла
+
+# получение данных от пользователя
+op_mode = input("Введите режим работы интерфейса (access/trunk): ")
+intf = input("Введите тип и номер интерфейса: ")
+vlans = input(mode[op_mode][1])
+
+# вывод информации
+print(f"interface {intf}\n" + "\n".join(mode[op_mode][0]).format(vlans))
