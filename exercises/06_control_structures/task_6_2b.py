80729f45100a19147961235ae90aad0d4eb422d1
Ivan <ghostmorgan13@gmail.com>
2021-11-30 20:21:39 +0300

    Решил задание 6.2b

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,33 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+correct_ip = False
+
+while not correct_ip:
+    ip_address = input("Введите IP-адрес в формате 10.0.1.1: ") # получаем адрес от пользователя
+    ip_list = ip_address.split('.') # получаем список октетов по разделителю "точка"
+    correct_ip = True
+
+    if len(ip_list) == 4: # проверка на кол-во октетов
+        for oct in ip_list: # цикл для проверки каждого октета условия того, что октет содержит только числа в диапазоне 0-255
+            if oct.isdigit() and int(oct) in range(256):
+                continue
+            else: # иначе выкидываем ошибку на предмет того, что октет содержит не только числа или находится не в диапазоне 0-255 и запрашиваем снова адрес
+                print("Неправильный IP-адрес")
+                correct_ip = False
+                break
+    else: # выкидываем ошибку в случае если кол-во октетов больше 4 и запрашиваем снова адрес
+        print("Неправильный IP-адрес")
+        correct_ip = False
+    
+# блок на определение типа адреса, выполняется только после получения корректного адреса
+if ip_address == '255.255.255.255':
+    print("local broadcast")
+elif ip_address == '0.0.0.0':
+    print("unassigned")
+elif int(ip_address.split('.')[0]) in range(1, 224):
+    print("unicast")
+elif int(ip_address.split('.')[0]) in range(224, 240):
+    print("multicast")
+else:
+    print("unused")
