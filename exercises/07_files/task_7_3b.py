28b70ce99a283bd1d238d5ba5201e97f4fec0a34
Ivan <ghostmorgan13@gmail.com>
2021-12-18 23:57:10 +0300

    Решил задание 7.3b

--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -17,3 +17,14 @@ Enter VLAN number: 10
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+mac_template = """
+{0:<10}{1:<20}{2}
+"""
+user_vlan = int(input("Enter VLAN number: "))
+
+with open('CAM_table.txt') as cam_table:
+    for line in cam_table:
+        mac_line = line.split()
+        if mac_line and mac_line[0].isdigit() and user_vlan == int(mac_line[0]):
+            print(mac_template.format(mac_line[0], mac_line[1], mac_line[3]).lstrip().rstrip())
