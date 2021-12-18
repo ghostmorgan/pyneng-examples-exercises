b8c6533707a6936c0ee0b949b858aa94f66c35ba
Ivan <ghostmorgan13@gmail.com>
2021-12-18 19:44:51 +0300

    Решил задание 7.1

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -14,3 +14,17 @@ Outbound Interface    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+template = """
+    Prefix                {0:<15}
+    AD/Metric             {1:<15}
+    Next-Hop              {2:<15}
+    Last update           {3:<15}
+    Outbound Interface    {4:<15}
+"""
+
+with open('ospf.txt', 'r') as f:
+    for line in f:
+        ospf = line.split()
+        prefix, ad, next_hop, update, out_intf = ospf[1], ospf[2].strip('[]'), ospf[4].strip(','), ospf[5].strip(','), ospf[6]
+        print(template.format(prefix, ad, next_hop, update, out_intf))
