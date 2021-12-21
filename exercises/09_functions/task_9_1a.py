56ca7a376f832ec7d94e869c80cff04949a0b590
Ivan <ghostmorgan13@gmail.com>
2021-12-22 02:32:22 +0300

    Решил задание 9.1а

--- exercises/09_functions/task_9_1a.py
+++ exercises/09_functions/task_9_1a.py
@@ -43,3 +43,27 @@ port_security_template = [
 ]
 
 access_config = {"FastEthernet0/12": 10, "FastEthernet0/14": 11, "FastEthernet0/16": 17}
+
+def generate_access_config(intf_vlan_mapping, access_template, psecurity=None):
+    """
+    intf_vlan_mapping - словарь с соответствием интерфейс-VLAN такого вида:
+        {'FastEthernet0/12':10,
+         'FastEthernet0/14':11,
+         'FastEthernet0/16':17}
+    access_template - список команд для порта в режиме access
+
+    Возвращает список всех портов в режиме access с конфигурацией на основе шаблона
+    """
+    access_result = []
+
+    for intf, vlan in intf_vlan_mapping.items():
+        access_result.append(f"interface {intf}")
+        for line in access_template:
+            if "vlan" in line:
+                access_result.append(f"{line} {vlan}")
+            else:
+                access_result.append(line)
+        if psecurity:
+            access_result.extend(psecurity)
+    
+    return access_result
