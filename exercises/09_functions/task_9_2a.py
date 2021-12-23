143549cd1b0de408614a6df946455883b6a2ef6b
Ivan <ghostmorgan13@gmail.com>
2021-12-24 00:02:59 +0300

    Решил задание 9.2a

--- exercises/09_functions/task_9_2a.py
+++ exercises/09_functions/task_9_2a.py
@@ -47,3 +47,19 @@ trunk_config = {
     "FastEthernet0/2": [11, 30],
     "FastEthernet0/4": [17],
 }
+
+def generate_trunk_config(intf_vlan_mapping, trunk_template):
+  """
+  Функция, генерирующая шаблон для trunk интерфейсов
+  """
+  trunk_result_dict = {}
+
+  for intf, vlans in intf_vlan_mapping.items():
+    trunk_result_dict[intf] = []
+    for command in trunk_template:
+      if "allowed" in command:
+        trunk_result_dict[intf].append(f"{command} {','.join([str(vlan) for vlan in vlans])}")
+      else:
+        trunk_result_dict[intf].append(command)
+  
+  return trunk_result_dict
