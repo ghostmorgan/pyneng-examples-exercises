69aa3ca0a2defd9292069f8a0252dba2af8a758c
Ivan <ghostmorgan13@gmail.com>
2021-12-26 21:43:52 +0300

    Решил задание 11.1

--- exercises/11_modules/task_11_1.py
+++ exercises/11_modules/task_11_1.py
@@ -42,7 +42,77 @@ def parse_cdp_neighbors(command_output):
     вместо имени файла, мы делаем функцию более универсальной: она может работать
     и с файлами и с выводом с оборудования.
     Плюс учимся работать с таким выводом.
+    """    
+    list_config = list(filter(None, command_output.strip().split('\n'))) #формирование списка строк из строки вывода команды show cdp neighbors (удаляет переносы и пустые строки) 
+    
+    list_devices = cdp_list(list_config)
+    dict_devices = cdp_dict(list_devices)
+    
+    return dict_devices
+
+
+def cdp_list(list_config):
     """
+    Функция формирующая два списка для словаря из списка строк:
+
+    На вход принимает строку - вывод команды show cdp neighbors
+
+    На выходе возвращает список кортежей (device, interface)
+        * нечетный элемент - локальное устройство с соответствующим интерфейсом
+        * четный элемент - соседнее устройство с соответствующим интерфейсом
+    """
+
+    l_keys = []
+    l_values = []
+
+    for line in list_config:
+        if "show" in line:
+            local_device = line.split('>')[0]
+        elif num_check(line):
+            neighbor = line.strip().split()
+            l_keys.append(local_device)
+            l_values.append(neighbor[1] + neighbor[2])
+            l_keys.append(neighbor[0])
+            l_values.append(neighbor[-2] + neighbor[-1])
+    
+    result_list = list(zip(l_keys, l_values))
+
+    return result_list
+
+
+def num_check(line):
+    """
+    Функция проверяющая есть ли в строке хотя бы одна цифра
+
+    На вход принимает строку line:
+        * возращает True - если в строке есть хотя бы одна цифра 
+        * возращает False - если в строке нет цифр
+    """
+    return any(char.isdigit() for char in line)
+
+
+def cdp_dict(list_neighbors):
+    """
+    Функция формирующая итоговый словарь соседей
+
+    На вход принимает строку список кортежей, в котором:
+        * нечетный элемент - локальное устройство с соответствующим интерфейсом
+        * четный элемент - соседнее устройство с соответствующим интерфейсом
+        
+    Возращает словарь кортежей в формате: 
+        {("R4", "Fa0/1"): ("R5", "Fa0/1"),
+        ("R4", "Fa0/2"): ("R6", "Fa0/0")}
+    """
+    result_dict = {}
+
+    for position, string in enumerate(list_neighbors):
+        if position % 2 == 0:
+            key = string
+            result_dict[string] = None
+        else:
+            result_dict[key] = string
+
+    return result_dict
 
 
 if __name__ == "__main__":
