5f83af935e9af1f3f011bf8a151b5a0a2ec20e4f
Ivan <ghostmorgan13@gmail.com>
2021-12-29 20:43:46 +0300

    Решил задание 12.3

--- exercises/12_useful_modules/task_12_3.py
+++ exercises/12_useful_modules/task_12_3.py
@@ -18,3 +18,15 @@ Reachable    Unreachable
              10.1.1.9
 
 """
+
+from tabulate import tabulate
+
+def print_ip_table(reachable_list, unreachable_list):
+    result_dict = {"Reachable": reachable_list, "Unreachable": unreachable_list}
+    print(tabulate(result_dict, headers='keys'))
+
+
+if __name__ == "__main__":
+    reachable = ["10.1.1.1", "10.1.1.2"]
+    unreachable = ["10.1.1.7", "10.1.1.8", "10.1.1.9"]
+    print_ip_table(reachable, unreachable)
