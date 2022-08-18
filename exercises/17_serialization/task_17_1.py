216e0587c87957095b983a950f45512fb105884a
Ivan <ghostmorgan13@gmail.com>
2022-08-18 19:51:49 +0300

    Started to solve task

--- exercises/17_serialization/task_17_1.py
+++ exercises/17_serialization/task_17_1.py
@@ -31,3 +31,41 @@ sw3,00:E9:22:11:A6:50,100.1.1.7,3,FastEthernet0/21
 sw2_dhcp_snooping.txt, sw3_dhcp_snooping.txt.
 
 """
+
+import csv
+from fileinput import filename
+from pprint import pprint
+import re
+
+list_of_filenames = ["sw1_dhcp_snooping.txt", "sw2_dhcp_snooping.txt", "sw3_dhcp_snooping.txt"]
+
+def write_dhcp_snooping_to_csv(filenames, output):
+    
+    regex_filename = (r"^[^_]*")                # регулярка для получения имени устройства из имени файла
+    regex_output = (r"^([0-9a-fA-F]:?){12}")    # регулярка для получения необходимых данных из вывода команд
+
+    list_filenames = []
+    headers = ["switch", "mac", "ip","vlan", "interface"]
+    
+    # получение списка имён устройств из имён исходных файлов
+    for filename in filenames:
+        m = re.search(regex_filename, filename)
+        if m:
+            list_filenames.append(m.group(0))
+
+    #получение данных из исходных данных
+    for file in filenames:
+        with open(file) as f:
+            for line in f:
+                res = re.search(regex_output, line)
+
+                if res:
+                    print(file + "   ---   " + res.group(0))
+    
+    # запись полученных данных в файл csv
+    with open(output, "w") as f:
+        print()
+        ###
+
+if __name__ == "__main__":
+    write_dhcp_snooping_to_csv(list_of_filenames, "result_dhcp_snooping.csv")
