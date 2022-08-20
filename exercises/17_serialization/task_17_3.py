845f163799ae96af5d9ba363ab191dd1c8d2c16c
Ivan <ghostmorgan13@gmail.com>
2022-08-20 14:29:02 +0300

    Solve task 17.3

--- exercises/17_serialization/task_17_3.py
+++ exercises/17_serialization/task_17_3.py
@@ -24,3 +24,28 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 
 Проверить работу функции на содержимом файла sh_cdp_n_sw1.txt
 """
+
+import re
+
+def parse_sh_cdp_neighbors(output):
+
+    # получаем с помощью регулярки имя устройства из вывода команды show cdp neighbor
+    regex_local_device = (r"(?P<device_name>.*)[>#]show")
+    device_name = re.search(regex_local_device, output).group("device_name")
+    
+    # получаем с помощью регулярки имя удаленных устройств, локальные интерфейсы и интерфейсы на удаленных устройствах 
+    output_dict= {}
+    regex_parse = (r"^(?P<remote_device>\S+)\s+(?P<local_intf>\S+ \d+\/\d+).* (?P<remote_intf>\S+ \S+\/\d+)$")
+    output_dict[device_name] = {}
+
+    matches = re.finditer(regex_parse, output, re.MULTILINE)
+    for match in matches:
+        output_dict[device_name].update({match.group('local_intf'): {match.group('remote_device'): match.group('remote_intf')}})
+        
+    return(output_dict)
+
+
+if __name__ == "__main__":
+    with open("sh_cdp_n_sw1.txt") as input_file:
+        output = input_file.read()
+        parse_sh_cdp_neighbors(output)
